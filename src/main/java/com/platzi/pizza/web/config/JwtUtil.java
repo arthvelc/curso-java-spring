2cf7a6b11ce9407c7d515dcf596aafffb0319950
Arturo Solares <asolares.devml@gmail.com>
2024-11-29 21:17:46 -0600

    feat: implementación de autenticación con JWT y controlador de autenticación
    - Actualización en :
      - Se agregó la dependencia  para la generación y validación de tokens JWT.
    
    - Creación de :
      - DTO que encapsula las credenciales de inicio de sesión ( y ).
    
    - Implementación de :
      - Métodos para crear, validar y obtener el nombre de usuario de un token JWT.
    
    - Modificaciones en :
      - Se permite acceso público a las rutas de autenticación ().
      - Configuración de un  para la gestión de autenticación.
      - Mantenimiento de la encriptación de contraseñas con .
    
    - Creación de :
      - Endpoint  para la autenticación de usuarios.
      - Generación de un token JWT al autenticar credenciales válidas.
      - Respuesta HTTP con el token JWT en el encabezado .
    
    Con estos cambios se añade soporte completo para autenticación basada en JWT, asegurando mayor flexibilidad y seguridad en la gestión de sesiones.

---
+++ src/main/java/com/platzi/pizza/web/config/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.platzi.pizza.web.config;
+
+import com.auth0.jwt.JWT; // Importa la clase JWT para crear y manipular tokens JWT
+import com.auth0.jwt.algorithms.Algorithm; // Importa la clase Algorithm para definir el algoritmo de firma
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.stereotype.Component; // Marca esta clase como un componente de Spring para que sea gestionada por el contenedor de Spring
+
+import java.util.Date; // Proporciona funcionalidades para trabajar con fechas
+import java.util.concurrent.TimeUnit; // Permite trabajar con unidades de tiempo
+
+@Component // Declara esta clase como un componente de Spring para ser utilizada como un bean en el contexto de la aplicación
+public class JwtUtil {
+    // Constante para la clave secreta utilizada en la firma de los tokens
+    private static final String SECRET_KEY = "pl4tzi_pizz4";
+
+    // Configuración del algoritmo de firma HMAC256 utilizando la clave secreta
+    private static final Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    /**
+     * Crea un token JWT basado en el nombre de usuario.
+     *
+     * @param username El nombre de usuario que será incluido en el token.
+     * @return Un token JWT firmado.
+     */
+    public String create(String username) {
+        return JWT.create() // Inicia la construcción del token JWT
+                .withSubject(username) // Define el "subject" (sujeto) del token como el nombre de usuario
+                .withIssuer("platzi-pizza") // Establece el "issuer" (emisor) del token
+                .withIssuedAt(new Date()) // Añade la fecha de emisión del token
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                // Establece la fecha de expiración del token como 15 días después del momento actual
+                .sign(ALGORITHM); // Firma el token utilizando el algoritmo configurado
+    }
+
+    public boolean isValid(String jwt){
+        try {
+            JWT.require(ALGORITHM).build().verify(jwt);
+
+            return true;
+        } catch (JWTVerificationException e){
+            return false;
+        }
+    }
+
+    public String getUsername(String jwt){
+        return JWT.require(ALGORITHM).build().verify(jwt).getSubject();
+    }
+}
--- src/main/java/com/platzi/pizza/web/config/SecurutyConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurutyConfig.java
@@ -1,46 +1,66 @@
 package com.platzi.pizza.web.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean; // Permite definir métodos que producen beans administrados por Spring
+import org.springframework.context.annotation.Configuration; // Marca esta clase como una configuración de Spring
+import org.springframework.http.HttpMethod; // Enum para los métodos HTTP (GET, POST, PUT, etc.)
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; // Habilita la seguridad basada en anotaciones a nivel de métodos
+import org.springframework.security.config.annotation.web.builders.HttpSecurity; // Proporciona una API para configurar la seguridad HTTP
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Implementación del algoritmo BCrypt para encriptar contraseñas
+import org.springframework.security.crypto.password.PasswordEncoder; // Interfaz para encriptar contraseñas
+import org.springframework.security.web.SecurityFilterChain; // Representa la cadena de filtros de seguridad
 
-import java.util.Locale;
-
-@Configuration
-@EnableMethodSecurity(securedEnabled = true)
+@Configuration // Marca esta clase como una configuración de seguridad
+@EnableMethodSecurity(securedEnabled = true) // Habilita la seguridad basada en anotaciones (@Secured, @PreAuthorize)
 public class SecurutyConfig {
+
+    /**
+     * Configura la cadena de filtros de seguridad para la aplicación.
+     *
+     * @param http El objeto HttpSecurity que permite configurar la seguridad HTTP.
+     * @return La cadena de filtros de seguridad configurada.
+     * @throws Exception Si ocurre un error durante la configuración.
+     */
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().and()
-                .authorizeHttpRequests()
+                .csrf().disable() // Desactiva la protección CSRF (Cross-Site Request Forgery)
+                .cors().and() // Habilita CORS (Cross-Origin Resource Sharing)
+                .authorizeHttpRequests() // Comienza la configuración de las reglas de autorización
+                .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "CUSTOMER")
+                // Permite el acceso a rutas de "customers" solo a usuarios con los roles "ADMIN" o "CUSTOMER"
                 .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+                // Permite el acceso a GET en "pizzas" a los roles "ADMIN" y "CUSTOMER"
                 .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
+                // Permite el acceso a POST en "pizzas" solo a "ADMIN"
                 .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                // Permite el acceso a cualquier método PUT solo a "ADMIN"
                 .requestMatchers("/api/orders/random").hasAnyAuthority("random_order")
+                // Permite el acceso a "/orders/random" solo a usuarios con la autoridad "random_order"
                 .requestMatchers(HttpMethod.GET, "/api/orders/**").hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
+                // Permite el acceso a GET en "orders" solo a "ADMIN"
+                .anyRequest().authenticated()
+                // Requiere autenticación para cualquier otra solicitud
                 .and()
-                .httpBasic();
+                .httpBasic(); // Habilita autenticación HTTP básica
 
-        return http.build();
+        return http.build(); // Construye y devuelve la cadena de filtros configurada
     }
 
     @Bean
-    public PasswordEncoder encoder(){
-        return new BCryptPasswordEncoder();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    /**
+     * Define el bean para el encriptador de contraseñas.
+     *
+     * @return Un PasswordEncoder basado en BCrypt.
+     */
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(); // Retorna una implementación de BCrypt para encriptar contraseñas
     }
 }
