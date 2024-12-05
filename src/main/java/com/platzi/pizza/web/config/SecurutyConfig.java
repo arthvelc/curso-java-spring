e5fbdd59a35533f2a3a7f770ea24334f56c6491b
Arturo Solares <asolares.devml@gmail.com>
2024-12-04 18:48:19 -0600

    feat: Integrar filtro JWT en la configuración de seguridad
    - **JwtFilter**:
      - Importación de  para agregar detalles de autenticación al token.
      - Se añadió  para completar la información de autenticación con datos adicionales del request.
      - Se agregó un log con  para depurar el token de autenticación.
    
    - **SecurutyConfig**:
      - Integración del filtro  en la cadena de filtros de seguridad con .
      - Configuración de la política de manejo de sesiones a  para garantizar que no se utilicen sesiones en el servidor.
      - Modificaciones para habilitar el filtro de autenticación JWT antes del filtro .
    
    Esta actualización asegura que las solicitudes autenticadas mediante JWT sean manejadas de forma adecuada y sin estado.

--- src/main/java/com/platzi/pizza/web/config/SecurutyConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurutyConfig.java
@@ -1,5 +1,6 @@
 package com.platzi.pizza.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean; // Permite definir métodos que producen beans administrados por Spring
 import org.springframework.context.annotation.Configuration; // Marca esta clase como una configuración de Spring
 import org.springframework.http.HttpMethod; // Enum para los métodos HTTP (GET, POST, PUT, etc.)
@@ -7,13 +8,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity; // Habilita la seguridad basada en anotaciones a nivel de métodos
 import org.springframework.security.config.annotation.web.builders.HttpSecurity; // Proporciona una API para configurar la seguridad HTTP
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Implementación del algoritmo BCrypt para encriptar contraseñas
 import org.springframework.security.crypto.password.PasswordEncoder; // Interfaz para encriptar contraseñas
 import org.springframework.security.web.SecurityFilterChain; // Representa la cadena de filtros de seguridad
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // Marca esta clase como una configuración de seguridad
 @EnableMethodSecurity(securedEnabled = true) // Habilita la seguridad basada en anotaciones (@Secured, @PreAuthorize)
 public class SecurutyConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Autowired
+    public SecurutyConfig(JwtFilter jwtFilter){
+        this.jwtFilter = jwtFilter;
+    }
 
     /**
      * Configura la cadena de filtros de seguridad para la aplicación.
@@ -26,7 +36,10 @@ public class SecurutyConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // Desactiva la protección CSRF (Cross-Site Request Forgery)
-                .cors().and() // Habilita CORS (Cross-Origin Resource Sharing)
+                .cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeHttpRequests() // Comienza la configuración de las reglas de autorización
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/customers/**").hasAnyRole("ADMIN", "CUSTOMER")
@@ -44,7 +57,7 @@ public class SecurutyConfig {
                 .anyRequest().authenticated()
                 // Requiere autenticación para cualquier otra solicitud
                 .and()
-                .httpBasic(); // Habilita autenticación HTTP básica
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build(); // Construye y devuelve la cadena de filtros configurada
     }
